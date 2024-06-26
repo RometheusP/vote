17d764cb455bcd35fd4e6c08d156e6b2f8ea0280
Veigar <pwjaimer@163.com>
2024-06-26 17:36:00 +0800

    修改视图函数 增加登陆验证

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 from django.http import JsonResponse, HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 
-from polls.models import Subject, Teacher
+from polls.models import Subject, Teacher, User
+from polls.utils import gen_md5_digest
 
 
 def show_subjects(request):
@@ -10,38 +11,73 @@ def show_subjects(request):
 
 
 def show_teachers(request):
-    try:
-        sno = int(request.GET.get('sno'))
-        teachers = []
-        if sno:
-            subject = Subject.objects.only('name').get(no=sno)
-            teachers = Teacher.objects.filter(subject=subject).order_by('no')
-        return render(request, 'teachers.html', {
-            'subject': subject,
-            'teachers': teachers
-        })
-    except (ValueError, Subject.DoesNotExist):
-        return redirect('/')
+    if request.session.get('userid'):
+        try:
+            sno = int(request.GET.get('sno'))
+            teachers = []
+            if sno:
+                subject = Subject.objects.only('name').get(no=sno)
+                teachers = Teacher.objects.filter(subject=subject).order_by('no')
+            return render(request, 'teachers.html', {
+                'subject': subject,
+                'teachers': teachers
+            })
+        except (ValueError, Subject.DoesNotExist):
+            return redirect('/')
+    else:
+        data = {'code': 20002, 'mesg': '请先登录'}
+        return JsonResponse(data)
+        # return render(request, 'login.html', {'hint': "请先登录"})
 
 
-def praise_or_criticize(request):
-    """好评"""
-    try:
-        tno = int(request.GET.get('tno'))
-        teacher = Teacher.objects.get(no=tno)
-        if request.path.startswith('/praise'):
-            teacher.good_count += 1
-            count = teacher.good_count
-        else:
-            teacher.bad_count += 1
-            count = teacher.bad_count
-        teacher.save()
-        data = {'code': 20000, 'mesg': '操作成功', 'count': count}
-    except (ValueError, Teacher.DoseNotExist):
-        data = {'code': 20001, 'mesg': '操作失败'}
+def praise_or_criticize(request: HttpRequest) -> HttpResponse:
+    if request.session.get('userid'):
+        try:
+            tno = int(request.GET.get('tno'))
+            teacher = Teacher.objects.get(no=tno)
+            if request.path.startswith('/praise/'):
+                teacher.good_count += 1
+                count = teacher.good_count
+            else:
+                teacher.bad_count += 1
+                count = teacher.bad_count
+            teacher.save()
+            data = {'code': 20000, 'mesg': '投票成功', 'count': count}
+        except (ValueError, Teacher.DoesNotExist):
+            data = {'code': 20001, 'mesg': '投票失败'}
+    else:
+        data = {'code': 20002, 'mesg': '请先登录'}
     return JsonResponse(data)
 
 
 def login(request: HttpRequest) -> HttpResponse:
     hint = ''
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username and password:
+            password = gen_md5_digest(password)
+            user = User.objects.filter(username=username, password=password).first()
+            if user:
+                request.session['userid'] = user.no
+                request.session['username'] = user.username
+                return redirect('/')
+            else:
+                hint = '用户名或密码错误'
+        else:
+            hint = '请输入有效的用户名和密码'
     return render(request, 'login.html', {'hint': hint})
+
+
+def logout(request):
+    """注销"""
+    request.session.flush()
+    return redirect('/')
+
+
+def get_captcha(request: HttpRequest) -> HttpResponse:
+    """验证码"""
+    captcha_text = gen_random_code()
+    request.session['captcha'] = captcha_text
+    image_data = Captcha.instance().generate(captcha_text)
+    return HttpResponse(image_data, content_type='image/png')
